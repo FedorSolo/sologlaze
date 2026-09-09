9463d93308b8a7f666fd37d8405324a4c3ec1874
FedorSolo <Fedorsolo@ya.ru>
2026-09-09 18:49:57 -0300

    new

--- sologlazes/lib/queries/collections.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/lib/prisma";
-
-export async function getCollections() {
-  return prisma.collection.findMany({ orderBy: { sortOrder: "asc" } });
-}
-
-export async function getCollectionBySlug(slug: string) {
-  return prisma.collection.findUnique({ where: { slug } });
-}
